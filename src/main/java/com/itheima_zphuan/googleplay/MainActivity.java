9fd20c06859362f3b55b362b1c781226e4a564a2
钟佩桓 <395067061@qq.com>
2017-02-27 00:42:51 +0800

    创建工程，建立包，书写和导入工具类

--- src/main/java/com/itheima_zphuan/googleplay/MainActivity.java
+++ src/main/java/com/itheima_zphuan/googleplay/MainActivity.java
@@ -3,11 +3,19 @@ package com.itheima_zphuan.googleplay;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.socks.library.KLog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        KLog.d();
+        KLog.i();
+        KLog.e();
+        KLog.a();
+        KLog.v();
+        KLog.w();
     }
 }
