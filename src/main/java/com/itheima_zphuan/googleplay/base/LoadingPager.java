338f9ae95942438c996e5a65d7290d072ba0cd7f
zhongpeihuan <zphuanlove@gmail.com>
2017-02-28 00:07:11 +0800

    LoadingPager_完成数据触发加载之后的操作

--- src/main/java/com/itheima_zphuan/googleplay/base/LoadingPager.java
+++ src/main/java/com/itheima_zphuan/googleplay/base/LoadingPager.java
@@ -15,7 +15,7 @@ import com.itheima_zphuan.googleplay.utils.UIUtils;
  *      2.加载数据
  *      3.数据和视图的绑定
  */
-public class LoadingPager extends FrameLayout {
+public abstract class LoadingPager extends FrameLayout {
 
     public static final int STATE_LOADING = 0;//加载中
     public static final int STATE_ERROR = 1;//错误
@@ -26,6 +26,7 @@ public class LoadingPager extends FrameLayout {
     private View mLoadingView;
     private View mErrorView;
     private View mEmptyView;
+    private View mSuccessView;
 
     public LoadingPager(Context context) {
         super(context);
@@ -57,6 +58,8 @@ public class LoadingPager extends FrameLayout {
     /**
      * @des 根据状态刷新ui(决定LoadingPager到底提供四种视图中的哪一种)
      * @called 1.LoadingPager创建的时候
+     * @called 2.外界调用了triggerLoadData加载数据, 数据加载之前
+     * @called 3.外界调用了triggerLoadData加载数据, 而且数据加载完成了
      */
     private void refreshViewByState() {
         //控制 加载中视图 显示隐藏
@@ -79,8 +82,63 @@ public class LoadingPager extends FrameLayout {
         } else {
             mEmptyView.setVisibility(View.GONE);
         }
+        //这里就可能有成功视图了.因为数据已经加载完成了.而且数据加载成功了
+        if(mSuccessView == null && mCurState == STATE_SUCCESS){
+            mSuccessView = initSuccessView();
+            this.addView(mSuccessView);
+        }
+        // 控制 成功视图的 显示隐藏
+        if(mSuccessView!=null){
+            if(mCurState == STATE_SUCCESS){
+                mSuccessView.setVisibility(VISIBLE);
+            }else{
+                mSuccessView.setVisibility(GONE);
+            }
+        }
 
     }
 
 
+    /**
+     * @des 触发加载数据
+     * @called 外界想让LoadingPager触发加载数据的时候调用
+     */
+    public void triggerLoadData(){
+        new Thread(new LoadDataTask()).start();
+    }
+
+
+    private class LoadDataTask implements Runnable {
+        @Override
+        public void run() {
+            //真正的在子线程中加载具体的数据-->得到数据
+            int tempState = initData();
+
+            //处理数据
+            mCurState = tempState;
+
+            // 刷新UI(决定到底提供4种视图中的哪一种视图)
+            refreshViewByState();//mCurState-->Int
+
+        }
+    }
+
+    /**
+     * @des 在子线程中真正的加载具体的数据
+     * @des 在LoadingPager中, 不知道如何具体加载数据, 交给子类,子类必须实现
+     * @des 必须实现, 但是不知道具体实现, 定义成为抽象方法, 交给子类具体实现
+     * @called triggerLoadData()方法被调用的时候
+     */
+
+    public abstract int initData();
+
+    /**
+     * @return
+     * @des 决定成功视图长什么样子(需要定义成功视图)
+     * @des 数据和视图的绑定过程
+     * @des 在LoadingPager中, 其实不知道成功视图具体长什么样子, 更加不知道视图和数据如何绑定, 交给子类, 必须实现
+     * @des 必须实现, 但是不知道具体实现, 定义成为抽象方法, 交给子类具体实现
+     * @called triggerLoadData()方法被调用, 而且数据加载完成了, 而且数据加载成功
+     */
+    protected abstract View initSuccessView();
 }
