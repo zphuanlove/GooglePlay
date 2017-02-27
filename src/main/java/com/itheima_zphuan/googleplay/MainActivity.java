5940e8d96073aa5a8a6798a0a84b8f4e975614e2
zhongpeihuan <395067061@qq.com>
2017-02-27 16:48:45 +0800

    引入drawerlayout 并与actionbar home键进行联动

--- src/main/java/com/itheima_zphuan/googleplay/MainActivity.java
+++ src/main/java/com/itheima_zphuan/googleplay/MainActivity.java
@@ -1,18 +1,32 @@
 package com.itheima_zphuan.googleplay;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.socks.library.KLog;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_drawerLayout)
+    DrawerLayout mainDrawerLayout;
+
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         initActionBar();
+        initActionBarDrawerToggle();
     }
 
     private void initActionBar() {
@@ -33,14 +47,29 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
+    /**
+     * 点击ActionBar的home按钮会执行该方法，所以在该方法中做判断
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 //点击返回键
-                Toast.makeText(this, "点击了返回键", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(this, "点击了返回键", Toast.LENGTH_SHORT).show();
+                toggle.onOptionsItemSelected(item);
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * 初始化ActionBarDrawerToggle
+     */
+    private void initActionBarDrawerToggle() {
+        toggle = new ActionBarDrawerToggle(this,mainDrawerLayout, R.string.open,R.string.close);
+        //同步状态方法-->替换默认回退部分的UI效果
+        toggle.syncState();
+        //设置drawerLayout的监听 --> DrawerLayout拖动的时候,toggle可以跟着改变ui
+        mainDrawerLayout.setDrawerListener(toggle);
     }
 }
