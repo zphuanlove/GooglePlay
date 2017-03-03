650564347d8fee9cc01d6707d781cfb76ab001a2
zhongpeihuan <zphuanlove@gmail.com>
2017-03-03 19:54:32 +0800

    抽取HomeProtocal

--- src/main/java/com/itheima_zphuan/googleplay/fragment/HomeFragment.java
+++ src/main/java/com/itheima_zphuan/googleplay/fragment/HomeFragment.java
@@ -3,27 +3,17 @@ package com.itheima_zphuan.googleplay.fragment;
 import android.view.View;
 import android.widget.ListView;
 
-import com.google.gson.Gson;
 import com.itheima_zphuan.googleplay.base.BaseFragment;
 import com.itheima_zphuan.googleplay.base.BaseHolder;
 import com.itheima_zphuan.googleplay.base.LoadingPager;
 import com.itheima_zphuan.googleplay.base.SuperBaseAdapter;
 import com.itheima_zphuan.googleplay.bean.HomeBean;
 import com.itheima_zphuan.googleplay.bean.ItemBean;
-import com.itheima_zphuan.googleplay.conf.Constants;
 import com.itheima_zphuan.googleplay.holder.HomeHolder;
-import com.itheima_zphuan.googleplay.utils.HttpUtils;
+import com.itheima_zphuan.googleplay.protocal.HomeProtocol;
 import com.itheima_zphuan.googleplay.utils.UIUtils;
-import com.socks.library.KLog;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 /**
  * author: 钟佩桓
@@ -40,51 +30,26 @@ public class HomeFragment extends BaseFragment {
      */
     @Override
     public LoadingPager.LoadedResult initData() {
+        /*--------------协议进行简单封装以后--------------*/
+        HomeProtocol protocol = new HomeProtocol();
         try {
-            //1.创建OkHttpClient对象
-            OkHttpClient okHttpClient = new OkHttpClient();
-            // 2.拼接要访问的URL
-            // 拼接URL：http://localhost:8080/GooglePlayServer/home
-            String url = Constants.URLS.BASEURL+"home";
-            //定义参数对应的map
-            Map<String,Object> params = new HashMap<>();
-            params.put("index",0);
-            //拼接参数信息 ？ index = 0
-            String urlParamsByMap = HttpUtils.getUrlParamsByMap(params);
-            url = url +"?" +urlParamsByMap;
-
-            //3.创建请求对象
-            Request request = new Request.Builder().get().url(url).build();
-            //发起请求-->同步请求
-            Response response = okHttpClient.newCall(request).execute();
-            //4.解析响应的结果
-            if(response.isSuccessful()){
-                String resJsonString = response.body().string();
-                KLog.json("resJsonString-->"+resJsonString);
-                /************Json解析*************/
-                Gson gson = new Gson();
-                HomeBean homeBean = gson.fromJson(resJsonString, HomeBean.class);
-
-
-                LoadingPager.LoadedResult result = checkResult(homeBean);
-                if (result != LoadingPager.LoadedResult.SUCCESS) {//说明homeBean有问题,homeBean==null
-                    return result;
-                }
-                result = checkResult(homeBean.list);
-                if (result != LoadingPager.LoadedResult.SUCCESS) {//说明list有问题,list.size==0
-                    return result;
-                }
-
-                //走到这里来说明是成功的
-                //保存数据到成员变量
-                mItemBeans = homeBean.list;
-                mPictures = homeBean.picture;
-                //返回相应的状态
-                return result;//successs的状态
-            }else{//响应没有成功
-                return LoadingPager.LoadedResult.ERROR;
+            HomeBean homeBean = protocol.loadData();
+            LoadingPager.LoadedResult result = checkResult(homeBean);
+            if (result != LoadingPager.LoadedResult.SUCCESS) {//说明homeBean有问题,homeBean==null
+                return result;
             }
-        } catch (IOException e) {
+            result = checkResult(homeBean.list);
+            if (result != LoadingPager.LoadedResult.SUCCESS) {//说明list有问题,list.size==0
+                return result;
+            }
+
+            //走到这里来说明是成功的
+            //保存数据到成员变量
+            mItemBeans = homeBean.list;
+            mPictures = homeBean.picture;
+            //返回相应的状态
+            return result;//successs的状态
+        } catch (Exception e) {//响应没有成功
             e.printStackTrace();
             return LoadingPager.LoadedResult.ERROR;
         }
